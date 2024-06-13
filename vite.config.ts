1e74ffc3569a8d50119c8d3996809552c6001f91
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-06-13 16:34:35 +0200

    added script to run and build maven jar
    Signed-off-by: Erik Jan de Wit <erikjan.dewit@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import { checker } from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({ typescript: true })],
   server: {
-    port: 8080,
+    origin: "http://localhost:5173",
+    port: 5173,
   },
+  base: "",
   build: {
+    manifest: true,
+    sourcemap: true,
     rollupOptions: {
+      input: "src/main.tsx",
+      external: ["react", "react/jsx-runtime", "react-dom"],
       treeshake: false,
     },
   },
